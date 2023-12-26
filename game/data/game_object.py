6cd4aad95e1f475759796862b54b72719b89adc3
timmych <chenxiaozheng@gmail.com>
2023-12-25 22:01:28 -0800

    add boss, and game end / restart

--- game/data/game_object.py
+++ game/data/game_object.py
@@ -1,6 +1,6 @@
 import random
-from typing import Tuple
 import pygame
+from typing import Tuple
 from pygame.surface import Surface
 from data.const_data import Constants
 
@@ -9,6 +9,9 @@ angry_images = []
 for i in range(ANGRY_CAT_COUNT):
     angry_images.append(pygame.image.load(f"images/angrypongpong{i}.png"))
 
+player_image = pygame.image.load("images/hennysteel.png")
+boss_cat_image = pygame.image.load("images/big_angry_cat.png")
+
 
 class GameObject:
     x: int
@@ -16,6 +19,7 @@ class GameObject:
     size: int
     speed: int
     surface: Surface
+    health: int
 
     def __init__(
         self,
@@ -24,12 +28,14 @@ class GameObject:
         size=Constants.DEFAULT_OBJECT_SIZE,
         speed=Constants.DEFAULT_OBJECT_SPEED,
         surface=None,
+        health=1,
     ):
         self.x = x
         self.y = y
         self.size = size
         self.speed = speed
         self.surface = surface
+        self.health = health
 
     def pos(self):
         return (self.x, self.y)
@@ -38,18 +44,31 @@ class GameObject:
         self.y += self.speed
 
     # returns True if colliding with the input object
-    def check_collide(self, pos: Tuple[int, int], size: Tuple[int, int]) -> bool:
+    def check_collide(self, another_object) -> bool:
+        pos = another_object.pos()
         return (
             pos[0] < self.x + self.size
-            and pos[0] + size[0] > self.x
+            and pos[0] + another_object.size > self.x
             and pos[1] < self.y + self.size
-            and pos[1] + size[1] > self.y
+            and pos[1] + another_object.size > self.y
         )
+
+    def check_collide_and_hit_if_so(self, another_object) -> bool:
+        checked = self.check_collide(another_object)
+        if checked:
+            self.hit()
+            another_object.hit()
+        return checked
+
+    def hit(self) -> int:
+        self.health -= 1
+        return self.health
+
+    def dead(self) -> bool:
+        return self.health < 1
 
 
 class Bullet(GameObject):
-    penetrable: bool
-
     def __init__(self, x, y, penetrable, surface):
         super().__init__(
             x,
@@ -57,11 +76,20 @@ class Bullet(GameObject):
             size=Constants.DEFAULT_BULLET_SIZE,
             speed=-Constants.DEFAULT_BULLET_SPEED,
             surface=surface,
+            health=9999 if penetrable else 1,
         )
-        self.penetrable = penetrable
 
-    def can_penetrate(self) -> bool:
-        return self.penetrable
+
+class Boss(GameObject):
+    def __init__(self, x, y):
+        super().__init__(
+            x=x,
+            y=y,
+            size=Constants.BOSS_OBJECT_SIZE,
+            surface=boss_cat_image,
+            speed=Constants.DEFAULT_OBJECT_SPEED * 0.5,
+            health=Constants.BOSS_OBJECT_HEALTH,
+        )
 
 
 class GameObjectFactory:
@@ -88,3 +116,13 @@ class GameObjectFactory:
             - 1
             + random.randint(0, 2) * (1 + game_loop_i / 200.0),
         )
+
+    @staticmethod
+    def create_player(x, y) -> GameObject:
+        return GameObject(
+            x=x, y=y, size=Constants.DEFAULT_PLAYER_SIZE, surface=player_image, speed=0
+        )
+
+    @staticmethod
+    def create_boss_cat(x, y) -> Boss:
+        return Boss(x, y)
--- game/mario2.py
+++ game/mario2.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 import random
-from data.game_object import GameObject, GameObjectFactory
+from data.game_object import Boss, GameObject, GameObjectFactory
 from data.const_data import Constants
 
 
@@ -9,35 +9,28 @@ def main():
     # Initialize Pygame
     pygame.init()
 
-    # Constants
-    SCREEN_WIDTH = 1024
-    SCREEN_HEIGHT = 768
-    PLAYER_SIZE = 40
-    FPS = 60
-
-    # Colors
-    WHITE = (255, 255, 255)
-    RED = (255, 0, 0)
-
     # Create the screen
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Mymario vs Angry Pong")
+    screen = pygame.display.set_mode((Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT))
+    pygame.display.set_caption("Henny Steel Rabbit vs Angry Cat")
 
     # Load images
     background_image = pygame.image.load("images/back.png")
-    player_image = pygame.image.load("images/hennysteel.png")
 
     # Player variables
-    player_x = (SCREEN_WIDTH - PLAYER_SIZE) // 2
-    player_y = SCREEN_HEIGHT - PLAYER_SIZE - 10
+    player = GameObjectFactory.create_player(
+        x=(Constants.SCREEN_WIDTH - Constants.DEFAULT_PLAYER_SIZE) // 2,
+        y=Constants.SCREEN_HEIGHT - Constants.DEFAULT_PLAYER_SIZE - 10,
+    )
 
     # Object variables - list of GameObject
     objects = []
     bullets = []
-    object_speed = 5
     object_spawn_timer = 0
     object_spawn_interval = 5
-    bullet_ready = False
+
+    # is the game done?
+    game_on = True
+    game_won = False
 
     # Score variables
     score = 0
@@ -59,87 +52,141 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     bullets.append(
-                        GameObjectFactory.create_regular_bullet(x=player_x, y=player_y)
+                        GameObjectFactory.create_regular_bullet(x=player.x, y=player.y)
                     )
                 elif event.key == pygame.K_x:
                     bullets.append(
                         GameObjectFactory.create_penetrable_bullet(
-                            x=player_x, y=player_y
+                            x=player.x, y=player.y
                         )
                     )
+                elif not game_on and event.key == pygame.K_r:
+                    # reset everything
+                    # todo: put this into a common class / function
+                    player = GameObjectFactory.create_player(
+                        x=(Constants.SCREEN_WIDTH - Constants.DEFAULT_PLAYER_SIZE) // 2,
+                        y=Constants.SCREEN_HEIGHT - Constants.DEFAULT_PLAYER_SIZE - 10,
+                    )
+                    objects = []
+                    bullets = []
+                    object_spawn_timer = 0
+                    score = 0
+                    game_on = True
+                    game_won = False
+                    clock = pygame.time.Clock()
+                    game_loop_counter = 0
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and player_x > 0:
-            player_x -= 6
-        if keys[pygame.K_RIGHT] and player_x < SCREEN_WIDTH - PLAYER_SIZE:
-            player_x += 6
-        if keys[pygame.K_UP] and player_y < SCREEN_HEIGHT - PLAYER_SIZE:
-            player_y -= 4
-        if keys[pygame.K_DOWN] and player_y >= 0:
-            player_y += 4
+        if keys[pygame.K_LEFT] and player.x > 0:
+            player.x -= 6
+        if (
+            keys[pygame.K_RIGHT]
+            and player.x < Constants.SCREEN_WIDTH - Constants.DEFAULT_PLAYER_SIZE
+        ):
+            player.x += 6
+        if (
+            keys[pygame.K_UP]
+            and player.y < Constants.SCREEN_HEIGHT - Constants.DEFAULT_PLAYER_SIZE
+        ):
+            player.y -= 4
+        if keys[pygame.K_DOWN] and player.y >= 0:
+            player.y += 4
 
-        object_spawn_timer += 1
-        game_loop_counter += 1
-        if object_spawn_timer == object_spawn_interval:
-            objects.append(
-                GameObjectFactory.create_angry_cat(
-                    x=random.randint(0, SCREEN_WIDTH - Constants.DEFAULT_OBJECT_SIZE),
-                    y=-Constants.DEFAULT_OBJECT_SIZE,
-                    game_loop_i=game_loop_counter,
+        if game_on:
+            object_spawn_timer += 1
+            game_loop_counter += 1
+            if object_spawn_timer == object_spawn_interval:
+                objects.append(
+                    GameObjectFactory.create_angry_cat(
+                        x=random.randint(
+                            0, Constants.SCREEN_WIDTH - Constants.DEFAULT_OBJECT_SIZE
+                        ),
+                        y=-Constants.DEFAULT_OBJECT_SIZE,
+                        game_loop_i=game_loop_counter,
+                    )
                 )
-            )
-            object_spawn_timer = 0
+                object_spawn_timer = 0
 
-        for obj in objects:
-            obj.move()
+            # create boss cat
+            if (
+                score > Constants.BOSS_START_SCORE
+                and score % Constants.BOSS_START_SCORE is 0
+            ):
+                objects.append(
+                    GameObjectFactory.create_boss_cat(
+                        x=random.randint(
+                            0, Constants.SCREEN_WIDTH - Constants.BOSS_OBJECT_SIZE
+                        ),
+                        y=-Constants.BOSS_OBJECT_SIZE,
+                    )
+                )
 
-        for blt in bullets:
-            blt.move()
+            bosses = [obj for obj in objects if isinstance(obj, Boss)]
 
-        objects = [obj for obj in objects if obj.y < SCREEN_HEIGHT]
-        bullets = [blt for blt in bullets if blt.y < SCREEN_HEIGHT]
+            for obj in objects:
+                obj.move()
 
-        used_bullets = []
+            for blt in bullets:
+                blt.move()
 
-        # Collision detection: bullets and objects
-        for blt in bullets:
-            old_objects_len = len(objects)
-            objects = [
-                obj
-                for obj in objects
-                if not blt.check_collide((obj.x, obj.y), (obj.size, obj.size))
-            ]
-            if len(objects) is not old_objects_len and not blt.can_penetrate():
-                used_bullets.append(blt)
-        # take out used bullets that can't penetrate
-        bullets = [blt for blt in bullets if blt not in used_bullets]
+            objects = [obj for obj in objects if obj.y < Constants.SCREEN_HEIGHT]
+            bullets = [blt for blt in bullets if blt.y < Constants.SCREEN_HEIGHT]
 
-        # Collision detection: objects and player
-        for obj in objects:
-            if obj.check_collide((player_x, player_y), (PLAYER_SIZE, PLAYER_SIZE)):
-                print("Game Over! Your Score:", score)
-                pygame.quit()
-                sys.exit()
+            # Collision detection: bullets and objects
+            for blt in bullets:
+                for obj in objects:
+                    blt.check_collide_and_hit_if_so(obj)
 
-        # Update score
-        score += 1
+            game_won = len(bosses) > 0 and any(b.dead() for b in bosses)
+
+            if game_won:
+                game_on = False
+                continue
+
+            objects = [obj for obj in objects if not obj.dead()]
+            bullets = [blt for blt in bullets if not blt.dead()]
+
+            # Collision detection: objects and player
+            for obj in objects:
+                if obj.check_collide(player):
+                    game_on = False
+
+            # Update score
+            score += 1
 
         # Draw everything
-        screen.blit(player_image, (player_x, player_y))
+        screen.blit(player.surface, (player.x, player.y))
         for obj in objects:
             screen.blit(obj.surface, obj.pos())
         for blt in bullets:
             screen.blit(blt.surface, blt.pos())
 
         # Display score
-        score_text = font.render(f"Score: {score}", True, WHITE)
+        score_text = font.render(f"Score: {score}", True, Constants.WHITE)
         screen.blit(score_text, (10, 10))
+
+        if not game_on:
+            show_game_end(screen, game_won, score)
 
         # Update the display
         pygame.display.flip()
 
         # Cap the frame rate
-        clock.tick(FPS)
+        clock.tick(Constants.FPS)
+
+
+def show_game_end(screen, game_won, score):
+    font = pygame.font.Font(None, 80)
+    win_msg = "YOU WIN!" if game_won else "You lost :("
+    score_text = font.render(
+        f"{win_msg} Score: {score}",
+        True,
+        Constants.GREEN if game_won else Constants.RED,
+    )
+    screen.blit(
+        score_text,
+        (Constants.SCREEN_WIDTH / 2 - 250, Constants.SCREEN_HEIGHT / 2 - 100),
+    )
 
 
 if __name__ == "__main__":
